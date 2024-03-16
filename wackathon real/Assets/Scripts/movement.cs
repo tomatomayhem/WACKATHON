66a558579ab01c60c8d3510fd7cb33950692f4e3
maddieplested <maddieplested04@gmail.com>
2024-03-16 19:27:53 +0000

    Testing

--- wackathon real/Assets/Scripts/movement.cs
+++ wackathon real/Assets/Scripts/movement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 2f; // Speed of movement
+    private Vector3 targetPosition; // Target position (left side of the screen)
+
     void Start()
     {
-        
+        // Set the target position to the left side of the screen
+        targetPosition = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, 10));
+        // Set the initial position to the center of the screen
+        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move the sprite towards the target position
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        // If the sprite reaches the target position, disable the script to stop the movement
+        if (transform.position == targetPosition)
+        {
+            enabled = false;
+        }
     }
 }
